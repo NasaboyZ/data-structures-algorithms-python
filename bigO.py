ea382aa1b93c4e84592cf11a4eab57316889d4b9
NazaboyZ <josefleite.00@hotmail.com>
2026-03-14 15:26:56 +0100

    chore implementet BigOn2

---
+++ bigO.py
@@ -0,0 +1,24 @@
+# O(n)
+def print_items(n):
+    for i in range (n):
+        print(i)
+
+
+print_items(10)
+
+
+# drop Const
+def print_items(n):
+    for i in range (n):
+        print(i)
+    for j in range (n):
+        print(j)
+
+print_items(10)
+
+# O(n^2)
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print (i,j)
+print_items(10)
